bc8059c201797cde28b6a88ce1e6b5de651c23bf
Russ Fellman <russ216@gmail.com>
2012-04-22 21:58:52 -0400

    Added files

---
+++ src/com/funcage/android/FunCageActivity.java
@@ -0,0 +1,13 @@
+package com.funcage.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FunCageActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
