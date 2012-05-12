2c252f9776066ae170d4839783c7cba575cdd73b
Russ Fellman <russ216@gmail.com>
2012-05-12 12:00:07 -0400

    changed portrait layout

--- gen/com/funcage/android/R.java
+++ gen/com/funcage/android/R.java
@@ -45,13 +45,14 @@ containing a value of this type.
         public static final int IAB_MRECT=0x7f040001;
         public static final int RelativeLayout1=0x7f040004;
         public static final int adView=0x7f040008;
+        public static final int buttons=0x7f040007;
         public static final int funcagebanner=0x7f040005;
-        public static final int getRandomButton=0x7f040007;
-        public static final int loadingText=0x7f04000b;
-        public static final int progressBar1=0x7f04000a;
+        public static final int getRandomButton=0x7f040009;
+        public static final int loadingText=0x7f04000c;
+        public static final int progressBar1=0x7f04000b;
         public static final int randomPicWebView=0x7f040006;
-        public static final int shareButton=0x7f040009;
-        public static final int textView1=0x7f04000c;
+        public static final int shareButton=0x7f04000a;
+        public static final int textView1=0x7f04000d;
     }
     public static final class layout {
         public static final int black=0x7f030000;
